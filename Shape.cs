5e9ccd821d8e11b48fe2e943fa113b9cf322be34
Baneslayer55 <65244708+Baneslayer55@users.noreply.github.com>
2020-05-12 19:45:53 +0300

    Add files via upload

---
+++ Shape.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ShapeAreaCalculation
+{
+    abstract class Shape
+    {
+        public abstract string ShapeName { get; set; }
+        public abstract double ShapeArea { get; }
+        public abstract bool ShapeCheck { get; }
+    }
+}
